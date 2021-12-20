10a7bd9e20e2d422779fc17038dac10a25b3b48d
welvery2010 <very20101@users.noreply.github.com>
2021-12-21 05:37:06 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,12 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo  'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo  'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+
+
+# themes
+git clone https://github.com/rosywrt/luci-theme-rosy/tree/openwrt-18.06/luci-theme-rosy.git package/luci-theme-rosy
+git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/luci-theme-infinityfreedom
+git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
+git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
