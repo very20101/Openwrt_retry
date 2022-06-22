1d4b2ecaeac12f3378b2d72515e982166491872b
welvery2010 <very20101@users.noreply.github.com>
2022-06-22 15:45:21 +0800

    Update and rename diy-part02new to diy-part02new.sh

--- diy-part02new.sh
+++ diy-part02new.sh
@@ -12,10 +12,7 @@
 
 # Modify default IP
   sed -i 's/192.168.1.1/192.168.1.100/g' package/base-files/files/bin/config_generate
+  
 # kernel
   #sed -i "s/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g" target/linux/armvirt/Makefile
    sed -i "s/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.15/g" target/linux/armvirt/Makefile
-
-#sagernet-core
-  #sed -i 's|$(LN) v2ray $(1)/usr/bin/xray|#$(LN) v2ray $(1)/usr/bin/xray|g' feeds/small8/sagernet-core/Makefile
-  #sed -i 's|CONFLICTS:=v2ray-core xray-core|#CONFLICTS:=v2ray-core xray-core|g' feeds/small8/sagernet-core/Makefile
