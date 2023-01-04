dd694defd5d3d4d63190f45919b9f34e95a5762f
welvery2010 <very20101@users.noreply.github.com>
2023-01-04 11:13:19 +0800

    Update and rename diy-scriptnova.sh to diy-script-nova.sh

--- diy-script-nova.sh
+++ diy-script-nova.sh
@@ -18,10 +18,10 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-#echo 'src-git packages https://github.com/openwrt/packages.git' >>feeds.conf.default
-#echo 'src-git luci https://github.com/openwrt/luci.git' >>feeds.conf.default
-#echo 'src-git routing https://git.openwrt.org/feed/routing.git' >>feeds.conf.default
-#echo 'src-git telephony https://git.openwrt.org/feed/telephony.git' >>feeds.conf.default
+echo 'src-git packages https://github.com/openwrt/packages.git' >>feeds.conf.default
+echo 'src-git luci https://github.com/openwrt/luci.git' >>feeds.conf.default
+echo 'src-git routing https://git.openwrt.org/feed/routing.git' >>feeds.conf.default
+echo 'src-git telephony https://git.openwrt.org/feed/telephony.git' >>feeds.conf.default
 echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
 
 ## Add deps
