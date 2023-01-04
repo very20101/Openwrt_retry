b660b223b5078253bce33cb550894ed55d5302b3
welvery2010 <very20101@users.noreply.github.com>
2023-01-04 08:51:07 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -19,7 +19,11 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo  'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 #echo  'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
- echo  'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
+echo 'src-git packages https://github.com/openwrt/packages.git' >>feeds.conf.default
+echo 'src-git luci https://github.com/openwrt/luci.git' >>feeds.conf.default
+echo 'src-git routing https://git.openwrt.org/feed/routing.git' >>feeds.conf.default
+echo 'src-git telephony https://git.openwrt.org/feed/telephony.git' >>feeds.conf.default
+echo  'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
 
 ## Add deps
 git clone -b master --single-branch https://github.com/LGA1150/openwrt-fullconenat package/fullconenat
