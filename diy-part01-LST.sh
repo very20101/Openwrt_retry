e47dcf9cc1edc67b13db6400e08e7e1a13bc2e61
welvery2010 <very20101@users.noreply.github.com>
2023-01-16 11:44:13 +0800

    Create diy-part01-LST.sh

---
+++ diy-part01-LST.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo  'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+#echo  'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+ echo  'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
+
+## Add deps
+# git clone -b master --single-branch https://github.com/LGA1150/openwrt-fullconenat package/fullconenat
+
+# themes
+git clone https://github.com/rosywrt/luci-theme-rosy/tree/openwrt-18.06/luci-theme-rosy.git package/luci-theme-rosy
+git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/luci-theme-infinityfreedom
+git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
+git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
