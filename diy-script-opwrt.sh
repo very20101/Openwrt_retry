eb21f6a6ef73766bca427526281361898f05a20b
welvery2010 <very20101@users.noreply.github.com>
2023-06-30 08:00:27 +0800

    Update diy-script-opwrt.sh

--- diy-script-opwrt.sh
+++ diy-script-opwrt.sh
@@ -18,7 +18,10 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo  'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 #echo  'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
- echo  'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
+#echo  'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
+
+# Add extra package
+git clone https://github.com/kenzok8/small-package package/small-package
 
 ## Add deps
 git clone -b master --single-branch https://github.com/LGA1150/openwrt-fullconenat package/fullconenat
