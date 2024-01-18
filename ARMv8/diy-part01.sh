808cf74a4b6fb854e21d3da44ac510dd369b21b4
welvery2010 <very20101@users.noreply.github.com>
2024-01-18 09:28:43 +0800

    Update diy-part01.sh

--- ARMv8/diy-part01.sh
+++ ARMv8/diy-part01.sh
@@ -23,6 +23,10 @@
 #echo  'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 #echo  'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
 
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
+
 
 # themes
 git clone https://github.com/rosywrt/luci-theme-rosy/tree/openwrt-18.06/luci-theme-rosy.git package/luci-theme-rosy
