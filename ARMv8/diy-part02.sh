1d96ce3a06baef663616b9b95c60e51242200545
welvery2010 <very20101@users.noreply.github.com>
2024-05-18 02:00:15 +0800

    Update diy-part02.sh

--- ARMv8/diy-part02.sh
+++ ARMv8/diy-part02.sh
@@ -54,12 +54,12 @@ sed -i 's/192.168.1.1/192.168.1.100/g' package/base-files/files/bin/config_gener
 #sed -i "s/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.15/g" target/linux/armvirt/Makefile
 
 ## replace libs for shadowsocks-libev error
-#rm -rf package/libs/mbedtls  package/libs/ustream-ssl package/libs/uclient
-#git clone -b main https://github.com/very20101/openwrt_retry package/op-retry
-#mv package/op-retry/libs/mbedtls package/libs/mbedtls
-#mv package/op-retry/libs/ustream-ssl package/libs/ustream-ssl
-#mv package/op-retry/libs/uclient package/libs/uclient
-#rm -rf package/op-retry
+rm -rf package/libs/mbedtls  package/libs/ustream-ssl package/libs/uclient
+git clone -b main https://github.com/very20101/openwrt_retry package/op-retry
+mv package/op-retry/libs/mbedtls package/libs/mbedtls
+mv package/op-retry/libs/ustream-ssl package/libs/ustream-ssl
+mv package/op-retry/libs/uclient package/libs/uclient
+rm -rf package/op-retry
 
 # replace golang
 rm -rf feeds/packages/lang/golang
