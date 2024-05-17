0a28aee74fc1fc8cf289d1fa017a4dd06ea23ac3
welvery2010 <very20101@users.noreply.github.com>
2024-05-17 22:20:34 +0800

    Update diy-part01.sh

--- ARMv8/diy-part01.sh
+++ ARMv8/diy-part01.sh
@@ -23,13 +23,13 @@
 #echo  'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 #echo  'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
 
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 
 
 # themes
-git clone https://github.com/rosywrt/luci-theme-rosy/tree/openwrt-18.06/luci-theme-rosy.git package/luci-theme-rosy
-git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/luci-theme-infinityfreedom
-git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
-git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
+#git clone https://github.com/rosywrt/luci-theme-rosy/tree/openwrt-18.06/luci-theme-rosy.git package/luci-theme-rosy
+#git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/luci-theme-infinityfreedom
+#git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
+#git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
