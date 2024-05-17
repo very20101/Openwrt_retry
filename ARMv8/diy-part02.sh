bd022fc102456f5767122de0358022606770e6be
welvery2010 <very20101@users.noreply.github.com>
2024-05-17 21:25:17 +0800

    Update diy-part02.sh

--- ARMv8/diy-part02.sh
+++ ARMv8/diy-part02.sh
@@ -40,16 +40,14 @@ git clone -b master --single-branch https://github.com/LGA1150/openwrt-fullconen
 merge_package https://github.com/kenzok8/small-package package/luci-app-dnscrypt-proxy2
 
 ## Add extra package
-git clone https://github.com/kenzok8/openwrt-packages package/openwrt-packages
-git clone https://github.com/kenzok8/small package/small
-rm -rf package/small/shadowsocks-rust
-merge_package https://github.com/xiaorouji/openwrt-passwall-packages package/small/shadowsocks-rust
-
-rm -rf feeds/small8/hysteria
-merge_package https://github.com/xiaorouji/openwrt-passwall-packages feeds/small8/hysteria
-rm -rf feeds/packages/net/dnscrypt-proxy2
-merge_package https://github.com/coolsnowwolf/packages/net/dnscrypt-proxy2 feeds/packages/net/dnscrypt-proxy2
-rm -rf package/openwrt-packages/adguardhome/patches
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/utils/v2dat
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+./scripts/feeds install -a
 
 ## Add modified config
 #rm -rf config/Config-images.in
@@ -63,16 +61,16 @@ sed -i 's/192.168.1.1/192.168.1.100/g' package/base-files/files/bin/config_gener
 #sed -i "s/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.15/g" target/linux/armvirt/Makefile
 
 # golang 
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+#rm -rf feeds/packages/lang/golang
+#git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
 
 ## replace libs for shadowsocks-libev error
-rm -rf package/libs/mbedtls  package/libs/ustream-ssl package/libs/uclient
-git clone -b main https://github.com/very20101/openwrt_retry package/op-retry
-mv package/op-retry/libs/mbedtls package/libs/mbedtls
-mv package/op-retry/libs/ustream-ssl package/libs/ustream-ssl
-mv package/op-retry/libs/uclient package/libs/uclient
-rm -rf package/op-retry
+#rm -rf package/libs/mbedtls  package/libs/ustream-ssl package/libs/uclient
+#git clone -b main https://github.com/very20101/openwrt_retry package/op-retry
+#mv package/op-retry/libs/mbedtls package/libs/mbedtls
+#mv package/op-retry/libs/ustream-ssl package/libs/ustream-ssl
+#mv package/op-retry/libs/uclient package/libs/uclient
+#rm -rf package/op-retry
 
   
 ./scripts/feeds update -a
