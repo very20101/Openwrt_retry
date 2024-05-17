724f76c0b42aa779311f600071d1742c6b7b723c
welvery2010 <very20101@users.noreply.github.com>
2024-05-17 22:49:14 +0800

    Update diy-part02.sh

--- ARMv8/diy-part02.sh
+++ ARMv8/diy-part02.sh
@@ -46,7 +46,7 @@ wget -P target/linux/generic/hack-5.10 https://raw.githubusercontent.com/coolsno
 
 ## Add deps
 #git clone -b master --single-branch https://github.com/LGA1150/openwrt-fullconenat package/fullconenat
-merge_package https://github.com/kenzok8/small-package/luci-app-dnscrypt-proxy2 package/luci-app-dnscrypt-proxy2
+#merge_package https://github.com/kenzok8/small-package/luci-app-dnscrypt-proxy2 package/luci-app-dnscrypt-proxy2
 
 # themes
 git clone https://github.com/rosywrt/luci-theme-rosy/tree/openwrt-18.06/luci-theme-rosy.git package/luci-theme-rosy
@@ -55,14 +55,12 @@ git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-th
 git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
 
 ## Add extra package
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
-./scripts/feeds update -a 
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default 
 rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
 rm -rf feeds/packages/utils/v2dat
-./scripts/feeds install -a 
-
 
 ## Add deps(from other source)
 merge_package https://github.com/coolsnowwolf/lede/trunk/package/lean/libcryptopp package/libcryptopp
@@ -79,13 +77,16 @@ sed -i 's/192.168.1.1/192.168.1.100/g' package/base-files/files/bin/config_gener
 #sed -i "s/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.15/g" target/linux/armvirt/Makefile
 
 ## replace libs for shadowsocks-libev error
-rm -rf package/libs/mbedtls  package/libs/ustream-ssl package/libs/uclient
-git clone -b main https://github.com/very20101/openwrt_retry package/op-retry
-mv package/op-retry/libs/mbedtls package/libs/mbedtls
-mv package/op-retry/libs/ustream-ssl package/libs/ustream-ssl
-mv package/op-retry/libs/uclient package/libs/uclient
-rm -rf package/op-retry
-  
+#rm -rf package/libs/mbedtls  package/libs/ustream-ssl package/libs/uclient
+#git clone -b main https://github.com/very20101/openwrt_retry package/op-retry
+#mv package/op-retry/libs/mbedtls package/libs/mbedtls
+#mv package/op-retry/libs/ustream-ssl package/libs/ustream-ssl
+#mv package/op-retry/libs/uclient package/libs/uclient
+#rm -rf package/op-retry
+
+# replace golang
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
