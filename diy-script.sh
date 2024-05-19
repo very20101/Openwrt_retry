a523465b90b268f62838474d1fbf554fe7c35a68
welvery2010 <very20101@users.noreply.github.com>
2024-05-20 03:12:43 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -31,7 +31,7 @@ echo "========================="
 #echo 'src-git luci https://github.com/openwrt/luci.git' >>feeds.conf.default
 #echo 'src-git routing https://git.openwrt.org/feed/routing.git' >>feeds.conf.default
 #echo 'src-git telephony https://git.openwrt.org/feed/telephony.git' >>feeds.conf.default
-#echo  'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
+git clone -b main  https://github.com/kenzok8/small-package package/smpackage
 
 ## Add deps
 git clone -b master --single-branch https://github.com/LGA1150/openwrt-fullconenat package/fullconenat
