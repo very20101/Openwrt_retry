2eae9e6fbfe97aaa96a68971ee3362443b77ad36
welvery2010 <very20101@users.noreply.github.com>
2024-07-09 13:35:35 +0800

    Update diy-part02.sh

--- ARMv8/diy-part02.sh
+++ ARMv8/diy-part02.sh
@@ -24,22 +24,22 @@
 #echo 'src-git routing https://git.openwrt.org/feed/routing.git' >>feeds.conf.default
 #echo 'src-git telephony https://git.openwrt.org/feed/telephony.git' >>feeds.conf.default
 # git clone   https://github.com/kenzok8/small-package package/smpackage
-git clone -b master https://github.com/kenzok8/openwrt-packages package/kenzo
-git clone -b master https://github.com/kenzok8/small package/small
+git clone  https://github.com/kenzok8/openwrt-packages packages/kenzo
+git clone  https://github.com/kenzok8/small packages/small
 
 ## Add deps
-git clone -b master --single-branch https://github.com/LGA1150/openwrt-fullconenat package/fullconenat
+git clone -b master --single-branch https://github.com/LGA1150/openwrt-fullconenat packages/fullconenat
 #merge_package https://github.com/kenzok8/small-package/luci-app-dnscrypt-proxy2 package/luci-app-dnscrypt-proxy2
 
 # themes
-git clone https://github.com/rosywrt/luci-theme-rosy/tree/openwrt-18.06/luci-theme-rosy.git package/luci-theme-rosy
-git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/luci-theme-infinityfreedom
-git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
-git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
+git clone https://github.com/rosywrt/luci-theme-rosy/tree/openwrt-18.06/luci-theme-rosy.git packages/luci-theme-rosy
+git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/sluci-theme-infinityfreedom
+git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git packages/luci-theme-opentomcat
+git clone https://github.com/sirpdboy/luci-theme-opentopd.git packages/luci-theme-opentopd
 
 ## Add extra package
-#git clone https://github.com/kenzok8/openwrt-packages package/openwrt-packages
-#git clone https://github.com/kenzok8/small package/small
+#git clone https://github.com/kenzok8/openwrt-packages packages/openwrt-packages
+#git clone https://github.com/kenzok8/small packages/small
 #rm -rf package/small/shadowsocks-rust
 #merge_package https://github.com/xiaorouji/openwrt-passwall-packages/shadowsocks-rust package/small/shadowsocks-rust
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
@@ -58,7 +58,7 @@ sed -i 's/192.168.1.1/192.168.1.100/g' package/base-files/files/bin/config_gener
 
 ## replace libs for shadowsocks-libev error
 #rm -rf package/libs/mbedtls  package/libs/ustream-ssl package/libs/uclient
-git clone -b main https://github.com/very20101/openwrt_retry package/op-retry
+git clone https://github.com/very20101/openwrt_retry packages/op-retry
 #mv package/op-retry/libs/mbedtls package/libs/mbedtls
 #mv package/op-retry/libs/ustream-ssl package/libs/ustream-ssl
 #mv package/op-retry/libs/uclient package/libs/uclient
@@ -68,14 +68,14 @@ git clone -b main https://github.com/very20101/openwrt_retry package/op-retry
 #mv package/op-retry/pkg_modified/curl feeds/packages/net/curl
 
 ## luci-app-dnscrypt-proxy2
-mv package/op-retry/pkg_modified/luci-app-dnscrypt-proxy2 package/luci-app-dnscrypt-proxy2
+mv packages/op-retry/pkg_modified/luci-app-dnscrypt-proxy2 packages/luci-app-dnscrypt-proxy2
 
 ## ruby 
 #rm -rf feeds/packages/lang/ruby
 #mv package/op-retry/pkg_modified/ruby feeds/packages/lang/ruby
 
 
-rm -rf package/op-retry
+rm -rf packages/op-retry
 
 # replace golang
 rm -rf feeds/packages/lang/golang
