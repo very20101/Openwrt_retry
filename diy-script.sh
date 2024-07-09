41500b3426089ecca0ecdf04c5ffc4923a6dc3b3
welvery2010 <very20101@users.noreply.github.com>
2024-07-09 11:28:43 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -31,9 +31,9 @@
 #echo 'src-git luci https://github.com/openwrt/luci.git' >>feeds.conf.default
 #echo 'src-git routing https://git.openwrt.org/feed/routing.git' >>feeds.conf.default
 #echo 'src-git telephony https://git.openwrt.org/feed/telephony.git' >>feeds.conf.default
-git clone -b main  https://github.com/kenzok8/small-package package/smpackage
-#git clone -b master https://github.com/kenzok8/openwrt-packages package/kenzo
-#git clone -b master https://github.com/kenzok8/small package/small
+#git clone -b main  https://github.com/kenzok8/small-package package/smpackage
+git clone -b master https://github.com/kenzok8/openwrt-packages package/kenzo
+git clone -b master https://github.com/kenzok8/small package/small
 
 ## Add deps
 git clone -b master --single-branch https://github.com/LGA1150/openwrt-fullconenat package/fullconenat
@@ -61,27 +61,27 @@ git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme
 sed -i 's/192.168.1.1/192.168.1.100/g' package/base-files/files/bin/config_generate
   
 # kernel
-sed -i "s/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=6.1/g" target/linux/armsr/Makefile
+#sed -i "s/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=6.1/g" target/linux/armsr/Makefile
 #sed -i "s/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g" target/linux/armvirt/Makefile
 #sed -i "s/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.15/g" target/linux/armvirt/Makefile
 
 ## replace libs for shadowsocks-libev error
-rm -rf package/libs/mbedtls  package/libs/ustream-ssl package/libs/uclient
-git clone -b main https://github.com/very20101/openwrt_retry package/op-retry
-mv package/op-retry/libs/mbedtls package/libs/mbedtls
-mv package/op-retry/libs/ustream-ssl package/libs/ustream-ssl
-mv package/op-retry/libs/uclient package/libs/uclient
+#rm -rf package/libs/mbedtls  package/libs/ustream-ssl package/libs/uclient
+#git clone -b main https://github.com/very20101/openwrt_retry package/op-retry
+#mv package/op-retry/libs/mbedtls package/libs/mbedtls
+#mv package/op-retry/libs/ustream-ssl package/libs/ustream-ssl
+#mv package/op-retry/libs/uclient package/libs/uclient
 
 ## curl
-rm -rf feeds/packages/net/curl
-mv package/op-retry/pkg_modified/curl feeds/packages/net/curl
+#rm -rf feeds/packages/net/curl
+#mv package/op-retry/pkg_modified/curl feeds/packages/net/curl
 
 ## luci-app-dnscrypt-proxy2
 mv package/op-retry/pkg_modified/luci-app-dnscrypt-proxy2 package/luci-app-dnscrypt-proxy2
 
 ## ruby 
-rm -rf feeds/packages/lang/ruby
-mv package/op-retry/pkg_modified/ruby feeds/packages/lang/ruby
+#rm -rf feeds/packages/lang/ruby
+#mv package/op-retry/pkg_modified/ruby feeds/packages/lang/ruby
 
 
 rm -rf package/op-retry
